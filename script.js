cbaadb0fe23f91992bf24a8d009a6e75644d4143
Thiago Nicolini <othiago.nicolini@gmail.com>
2026-08-06 10:37:58 -0300

    Começando o  js script
    até agora eu estou fazendo com que quando a pessoa clique em "enter", crie um novo checkbox.

---
+++ script.js
@@ -0,0 +1,8 @@
+const camposDeTarefas = document.querySelectorAll('.campo-tarefa');
+    camposDeTarefas.forEach(element => {
+        element.addEventListener('keydown', (event) =>{
+            if (event.key === 'Enter'){
+                console.log(event.target.value);''
+            }
+        })
+    });
