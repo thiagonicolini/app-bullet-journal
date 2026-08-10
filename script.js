bd3474193750b5acee31d07c6cb6753041448bcc
Thiago Nicolini <othiago.nicolini@gmail.com>
2026-08-10 11:03:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const camposDeTarefas = document.querySelectorAll('.campo-tarefa');
     camposDeTarefas.forEach(element => {
         element.addEventListener('keydown', (event) =>{
             if (event.key === 'Enter'){
-                console.log(event.target.value);''
+                console.log(event.target.value);
             }
         })
     });
