2171ed34f08862e3d22de57c56985d93b5f95ef4
Thiago Nicolini <othiago.nicolini@gmail.com>
2026-08-10 16:09:47 -0300

    criando nova tarefa quando a pessoa clicar "enter".

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const camposDeTarefas = document.querySelectorAll('.campo-tarefa');
-    camposDeTarefas.forEach(element => {
-        element.addEventListener('keydown', (event) =>{
-            if (event.key === 'Enter'){
-                console.log(event.target.value);
+document.addEventListener('keydown', (event) =>{
+            if (event.key === 'Enter' && event.target.classList.contains('campo-tarefa')){
+                const novoItem = document.createElement('li');
+                novoItem.classList.add('tarefa');
+                const listaDoDia = event.target.closest('ul');
+                novoItem.innerHTML = '<input type="checkbox"><input type="text" class="campo-tarefa" placeholder="Digite sua tarefa aqui...">';
+                listaDoDia.appendChild(novoItem);
             }
         })
-    });
